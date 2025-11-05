f207880b7529a7d08fe6e1572afd88772bac93bc
JoreMan <jorebeta@gmail.com>
2025-11-05 07:55:19 -0500

    Actualizacion de nuevos modulos

--- statics/js/modules/config.js
+++ statics/js/modules/config.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "https://backendtemporal.onrender.com"
+#export const BASE_URL = "https://backendtemporal.onrender.com" 
+export const BASE_URL = "https://backendtemporal.onrender.com" 
